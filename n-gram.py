2db61c710b2f7fb6373789b77b771ef6461a81c9
Burhan <2019cs626@student.uet.edu.pk>
2022-03-21 14:15:55 +0500

    Added more corpus

--- n-gram.py
+++ n-gram.py
@@ -38,7 +38,7 @@ for files in textFile:
     for sentence in temp:
         sentences.append(sentence)
 
-print(sentences)
+# print(sentences)
 
 words = []
 
@@ -46,28 +46,31 @@ for sentence in sentences:
     for x in splitToWords(sentence):
         words.append(x)
 
-print(len(words))
+# print(len(words))
 uniqueWords = set(words)
-print(len(uniqueWords))
+# print(len(uniqueWords))
 
 
 def getNGramString(string, ngramNumber):
     string = string.lower()
     string = splitToWords(string)
-    totalGrams = [string[0]]
-    for index, word in enumerate(string):
-        i = index
-        toFindProb = ''
-        if index > 0 and ngramNumber > 1:
-            for j in range(ngramNumber):
-                if i >= 0:
-                    toFindProb = toFindProb + string[i]
-                    toFindProb += ' '
-                    i -= 1
-        if toFindProb:
-            realWord = toFindProb.strip()
-            totalGrams.append(rev_words(realWord))
-    return totalGrams
+    if ngramNumber == 1:
+        return string
+    else:
+        totalGrams = [string[0]]
+        for index, word in enumerate(string):
+            i = index
+            toFindProb = ''
+            if index > 0 and ngramNumber > 1:
+                for j in range(ngramNumber):
+                    if i >= 0:
+                        toFindProb = toFindProb + string[i]
+                        toFindProb += ' '
+                        i -= 1
+            if toFindProb:
+                realWord = toFindProb.strip()
+                totalGrams.append(rev_words(realWord))
+        return totalGrams
 
 
 def findCount(word, corpus):
@@ -99,12 +102,13 @@ def getTotalProbability(sentence, corpus, ngram):
 
 
 # returns the probability of a sentence using bigram model
-def SentenceProb(sentence, corpus):
+def SentenceProb(sentence, corpus, totalWords):
     ngramString = getNGramString(sentence, 2)
     sentence = sentence.lower()
     sentence = sentence.split()
     totalProbability = []
-    totalProbability.append(findCount(sentence[0], corpus))
+    probOfFirstWord = findCountAtStart(sentence[0], corpus)/totalWords
+    totalProbability.append(probOfFirstWord)
     for i in range(1, len(sentence)):
         temp = findCount(ngramString[i], corpus)
         if temp:
@@ -116,12 +120,14 @@ def SentenceProb(sentence, corpus):
     return result
 
 
-def SmoothSentenceProb(sentence, corpus, uniqueWords):
+def SmoothSentenceProb(sentence, corpus, totalWords, uniqueWords):
     ngramString = getNGramString(sentence, 2)
     sentence = sentence.lower()
     sentence = sentence.split()
     totalProbability = []
-    totalProbability.append(findCount(sentence[0], corpus)+1)
+    probOfFirstWord = findCountAtStart(
+        sentence[0], corpus)+1/totalWords+uniqueWords
+    totalProbability.append(probOfFirstWord)
     for i in range(1, len(sentence)):
         temp = (findCount(ngramString[i], corpus)+1)
         result = temp/(findCount(sentence[i], corpus)+uniqueWords)
@@ -130,4 +136,15 @@ def SmoothSentenceProb(sentence, corpus, uniqueWords):
     return result
 
 
-# print(SmoothSentenceProb("The batman was a hit", sentences, len(uniqueWords)))
+def findCountAtStart(word, corpus):
+    count = 0
+    for sentence in corpus:
+        if sentence.startswith(word):
+            count += 1
+    return count
+
+
+# print(SmoothSentenceProb("The batman was a hit",
+#       sentences, len(words), len(uniqueWords)))
+
+print(getNGramString("The batman was a hit", 5))
